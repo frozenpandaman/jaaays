8554f5ffb31d4fa5909d6c32ab65289b3f96f4ca
maxhoward <mhoward@hmc.edu>
2016-11-12 05:46:38 -0800

    Added line length counting

--- jaaays.py
+++ jaaays.py
@@ -4,6 +4,13 @@ from PIL import ImageStat
 import os
 import sys
 import time
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+
 
 # (484) ALG-JAYS
 
@@ -64,6 +71,54 @@ def line():
 	print r2
 	return r2
 
+def lineCount():
+	hog = cv2.HOGDescriptor()
+	hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+	# load the image and resize it to (1) reduce detection time
+	# and (2) improve detection accuracy
+	image = cv2.imread(f)
+	image = imutils.resize(image, width=min(400, image.shape[1]))
+	orig = image.copy()
+
+	# detect people in the image
+	(rects, weights) = hog.detectMultiScale(image, winStride=(2, 2),
+		padding=(8, 8), scale=1.05)
+
+	# draw the original bounding boxes
+	for (x, y, w, h) in rects:
+		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+	# apply non-maxima suppression to the bounding boxes using a
+	# fairly large overlap threshold to try to maintain overlapping
+	# boxes that are still people
+	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+	## draw the final bounding boxes
+	# for (xA, yA, xB, yB) in pick:
+	# 	cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+	## show some information on the number of bounding boxes
+	# filename = imagePath[imagePath.rfind("/") + 1:]
+	# print("[INFO] {}: {} original boxes, {} after suppression".format(
+	# 	filename, len(rects), len(pick)))
+
+	# # show the output images
+	# cv2.imshow("Before NMS", orig)
+	# cv2.imshow("After NMS", image)
+	# cv2.waitKey(0)
+
+	HeadCount = len(pick)
+	if HeadCount == 0:
+		r3 = "There's nobody in line."
+	elif HeadCount == 1:
+		r3 = "There's 1 person in line."
+	else:
+		r3 = "There's about " + str(len(pick)) + " people in line."
+	print r3
+	return r3
+
 
 starttime = time.time()
 while True:
@@ -75,4 +130,8 @@ while True:
 		fi.write(r1)
 	with open('out_line.txt', 'wb') as fi:
 		fi.write(r2)
+	with open('out_count.txt', 'wb') as fi:
+		fi.write(r3)
 	time.sleep(1.0 - ((time.time() - starttime) % 1.0))
+
+
