e793c77cb58e2e208d57bf3d18cb95bddda07e44
Jackson Warley <jtwarley@gmail.com>
2016-11-12 07:21:04 -0800

    done with sms processing

--- sms.py
+++ sms.py
@@ -4,24 +4,38 @@ import twilio.twiml
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
+
 def send_report():
 
     VALID_COMMANDS = [ "open", "line" ]
 
     user_text = request.values.get('Body', None)
+    resp = twilio.twiml.Response()
 
-    open_file = lambda x: open("out_" + x + ".txt", 'r')
-
-    if user_text in VALID_COMMANDS:
-        out = open_file(user_text)
-        resp = twilio.twiml.Response()
-        resp.message(out.read())
-    else:
-        resp = twilio.twiml.Response()
-        outs = map(open_file, VALID_COMMANDS)
-        map(lambda x: resp.message(x.read()), outs)
+    if jays_is_open() and "open" in user_text:
+        process_sms_command("open", resp)
+    elif jays_is_open() and "line" in user_text:
+        print "line boop"
+        process_sms_command("line", resp)
+    elif not jays_is_open(): # closed
+        print "thinks it's clsoed"
+        process_sms_command("open", resp)
+    else: # unrecognized command and open
+        process_sms_command("open", resp)
+        process_sms_command("line", resp)
 
     return str(resp)
+
+def jays_is_open():
+    out = open("out_open.txt", 'r')
+    openness = (out.read().strip().split(" ")[-1] == "open!")
+    out.close()
+    return openness
+
+def process_sms_command(text, response):
+    out = open("out_" + text + ".txt", 'r')
+    response.message(out.read())
+    out.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
